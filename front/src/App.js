16920cbf5350febced9de91011bf1177d43a38f9
VisvShah <visvshah1@gmail.com>
2022-09-27 12:03:00 -0400

    Navbar Base Made

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-       
+       <Navbar/>
     </div>
   );
 }
--- front/src/Components/Navbar/navbar.jsx
+++ front/src/Components/Navbar/navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import "navbar.css"
+import "./navbar.scss"
 export default function navbar() {
   return (
     <div>
-        
+        <div className="navbar">
+            <div className="left">
+                BioBoards
+            </div>
+            <div className="right">
+                <a className = "link" href = '#landing'>Landing</a>
+                <a className = "link" href = '#drive'>Drive</a>
+            </div>
+        </div>
     </div>
   )
 }
