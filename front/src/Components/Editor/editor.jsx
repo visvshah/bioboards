e963086bd72d2e74215cdb3eea5660f816c8f014
VisvShah <visvshah1@gmail.com>
2022-10-24 14:19:26 -0400

    Added Editor

--- front/src/Components/Editor/editor.jsx
+++ front/src/Components/Editor/editor.jsx
@@ -1,34 +1,37 @@
-import React, { useEffect} from "react";
-import { useCanvas } from "./editorFunctions.js";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useRef} from "react";
+import { Editor } from '@tinymce/tinymce-react';
 import "./editor.scss";
-import Editor from "./editorFunctions.js";
 export default function () {
-    /*const {
-      canvasRef,
-      prepareCanvas,
-      startDrawing,
-      finishDrawing,
-      draw,
-    } = useCanvas();
-  
-    useEffect(() => {
-      prepareCanvas();
-    }, []);
-    */
+    const editorRef = useRef(null);
+    const log = () => {
+    if (editorRef.current) {
+        console.log(editorRef.current.getContent());
+    }
+  };
   return (
-    <div className="editor">
-      {/*<ReactQuill 
-              theme="snow" 
-              value={value} 
-              onChange={setValue} 
-              className = "quill"
-              
-          />
-  */}
-        <Editor/>
-    </div>
+    <div className="holder">
+      <Editor
+        className = "editor"
+        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+        onInit={(evt, editor) => editorRef.current = editor}
+        initialValue='<p>.             .</p>'
+        init={{
+          height: 600,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+          ],
+          toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help' + ' | insertfile image media template link anchor codesample',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+      <button className = 'save' onClick={log}>Save</button>
+      </div>
     
   )
 }
