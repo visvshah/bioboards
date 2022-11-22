2e2deb5b21310a7821e46c83fcc423a124b69b9b
VisvShah <visvshah1@gmail.com>
2022-11-22 17:54:55 -0500

    Updating Server index

--- server/index.js
+++ server/index.js
@@ -3,25 +3,18 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from "./Routes/userRoutes.js";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json());
-app.use(express.json({limit:'1mb'}))
-app.use(express.urlencoded({ extended: false}))
+app.use(bodyParser.json({limit:'5mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: false}));
 
-app.use((req, res, next) => {
-    console.log(req.url + ' : ' + req.body);
-    next();
-})
-app.use(bodyParser.json())
-try{
-    const conn = await mongoose.connect("mongodb+srv://VisvShah:VisvShah123@cluster0.umuygpo.mongodb.net/?retryWrites=true&w=majority")
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-} catch(error) {
-    console.log("NOPE");
-    console.log(error);
-    process.exit(1)
-}
-app.use('/api/users', userRoutes)
-app.listen(4000, () => console.log(`Server started on port ${4000}`))
+app.use(cors());
+app.use('/users', userRoutes)
+console.log("URL: " + process.env.CONNECTION_URL);
+app.listen(4000, () => console.log(`Server started on port ${4000}`));
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(4000, ()=> console.log(`Server runnning on port: 4000`)))
+    .catch((error)=> console.log(error.message));
